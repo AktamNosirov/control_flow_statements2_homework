34ceb5668ab680a8bfa49d18de56246afb5f80e9
AktamNosirov <94_aktam@mail.ru>
2022-03-25 21:57:31 +0500

    3m

--- if03.py
+++ if03.py
@@ -10,9 +10,10 @@ def main(a,b,c):
     """  
     if c>b>a or a>b>c :
         return b
-    if a>c>b or b>c>a :
+    elif a>c>b or b>c>a :
         return c
-    if b>a>c or c>a>b :
+    elif b>a>c or c>a>b :
         return a 
 print(main(5,7,1))
+
 
